a7c1fce56b362de281a5e2e92c23db485a2b57c2
bellvik <belvik058@gmail.com>
2025-03-23 17:24:33 +0500

    Update Solver.cs

--- Solver.cs
+++ Solver.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Data;
 using System.Linq;
+
 
 namespace GaussAlgorithm;
 
@@ -19,7 +21,12 @@ public class Solver
             }
             augmentedMatrix[i][m] = freeMembers[i];
         }
-
+        var rankMatrix = CalculateRank( ref matrix);
+        var rankAugmentedMatrix = CalculateRank(ref augmentedMatrix);
+        if (rankMatrix < rankAugmentedMatrix)
+        {
+            throw new NoSolutionException(matrix, freeMembers, augmentedMatrix);
+        }
         bool[] usedRows = new bool[n];
         for (int col = 0; col < m; col++)
         {
@@ -56,7 +63,6 @@ public class Solver
                 }
             }
         }
-            
 
         double[] solution = new double[m];
         for (int i = 0; i < m; i++)
@@ -87,5 +93,59 @@ public class Solver
         }
 
         return solution;
+    }
+    static int CalculateRank(ref double[][] m)
+    {
+        var matrix = m.Select(row => row.ToArray()).ToArray();
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        int rank = Math.Min(rows, cols); 
+        for (int row = 0; row < rank; row++)
+        {
+            if (matrix[row][row] == 0)
+            {
+                bool foundNonZero = false;
+                for (int i = row + 1; i < rows; i++)
+                {
+                    if (matrix[i][row] != 0)
+                    {
+                        SwapRows(matrix, row, i);
+                        foundNonZero = true;
+                        break;
+                    }
+                }
+                if (!foundNonZero)
+                {
+                    rank--;
+                    if (row < rank)
+                    {
+                        for (int i = 0; i < rows; i++)
+                        {
+                            matrix[i][row] = matrix[i][rank];
+                        }
+                    }
+                    row--;
+                    continue;
+                }
+            }
+
+            for (int i = row + 1; i < rows; i++)
+            {
+                double factor = matrix[i][row] / matrix[row][row];
+                for (int j = row; j < cols; j++)
+                {
+                    matrix[i][j] -= factor * matrix[row][j];
+                }
+            }
+        }
+
+        return rank;
+    }
+
+    static void SwapRows(double[][] matrix, int row1, int row2)
+    {
+        double[] temp = matrix[row1];
+        matrix[row1] = matrix[row2];
+        matrix[row2] = temp;
     }
 }
